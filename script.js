ed23678aa8a9e021616319548e7930ea0ff8a070
chotepurespirit <chovyzzz.nai@gmail.com>
2024-05-07 20:36:13 +0700

    add video in html and make the button clicking to play the video with js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+vdobutton = document.getElementById("video-btn");
+vdobox = document.getElementById("video-box");
+vdobutton.addEventListener("click", ()=>{
+    vdobox.innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/QbPtrnmGlZ8?si=xXUXTtYBnkqNaLKA\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
+});
+
+
+
 class StarRailChar {
   constructor(name, element, path) {
     this.name = name;
@@ -5,4 +13,3 @@ class StarRailChar {
     this.path = path;
   }
 }
-console.log(StarRailChar)
