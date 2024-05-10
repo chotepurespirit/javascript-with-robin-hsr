2b535a90a07754ee7e5bb863e2de75d187e7f58c
chotepurespirit <chovyzzz.nai@gmail.com>
2024-05-10 21:43:12 +0700

    add button my babe and add function clickchangecolor()

--- script.js
+++ script.js
@@ -4,12 +4,8 @@ vdobutton.addEventListener("click", ()=>{
     vdobox.innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/QbPtrnmGlZ8?si=xXUXTtYBnkqNaLKA\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
 });
 
-
-
-class StarRailChar {
-  constructor(name, element, path) {
-    this.name = name;
-    this.element =element;
-    this.path = path;
-  }
+function clickChangeColor() {
+  const title = document.getElementById("title");
+  title.innerHTML = "My Earnnie 💖💖💖";
+  title.style.color = "pink";
 }
